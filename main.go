2d59d46e8ad46718ef4877725967e479431da389
luis-octavius <luisoctavius.sc@gmail.com>
2025-10-13 18:33:46 -0300

    test TodoManager and its methods

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"todo-terminal/todo"
+	"fmt"
+)
+
 func main() {
+
+	manager := todo.NewTodoManager()
+
+	manager.AddTodo("Teste")
+
+	todo, exists := manager.GetTodo(1)
+	if exists {
+		fmt.Println(todo)
+	}
 
 }
--- todo/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/luis-octavius/todo
-
-go 1.25.1
