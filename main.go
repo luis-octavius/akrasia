b1806665cd5fe8f88583953bf3bec903e7e7ede5
luis-octavius <luisoctavius.sc@gmail.com>
2025-10-26 19:42:15 -0300

    test(TodoManager): test manually create and add a todo

--- main.go
+++ main.go
@@ -1,19 +1,11 @@
 package main
 
 import (
-	"todo-terminal/todo"
 	"fmt"
 )
 
 func main() {
 
-	manager := todo.NewTodoManager()
-
-	manager.AddTodo("Teste")
-
-	todo, exists := manager.GetTodo(1)
-	if exists {
-		fmt.Println(todo)
-	}
-
+	fmt.Println("Welcome...")
+	startRepl()
 }
