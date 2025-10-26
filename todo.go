ae31bf10c27eaaae2766e21e02b704f1bccd6f0d
luis-octavius <luisoctavius.sc@gmail.com>
2025-10-26 19:45:31 -0300

    feat(TodoManager): add a manager to manipulate and save the todos
    - Implement Get() to get a specific todo by id  - Implement GetAll() to get all todos (show them)  - Implement Delete() to delete a specific todo with ID  - Implement CreateTodoManager() to return a pointer to an instance of TodoManager  - Implement Todo and TodoManager struct

---
+++ todo.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type Todo struct {
+	id          int
+	description string
+	createdAt   time.Time
+	Status      bool
+}
+
+type TodoManager struct {
+	Todos map[int]Todo
+	Id    int
+}
+
+func CreateTodoManager() *TodoManager {
+	todoManager := TodoManager{
+		Todos: map[int]Todo{},
+		Id:    0,
+	}
+	return &todoManager
+}
+
+func (t TodoManager) Add(description string) error {
+	newTodo := Todo{
+		id:          t.Id,
+		description: description,
+		createdAt:   time.Now(),
+		Status:      false,
+	}
+
+	t.Todos[t.Id] = newTodo
+	t.Id++
+	fmt.Println("Todo added succesfully!")
+	return nil
+}
+
+func (t TodoManager) Get(id int) (Todo, error) {
+	todo, ok := t.Todos[id]
+	fmt.Println("Todo: ", todo)
+	if !ok {
+		return Todo{}, fmt.Errorf("Todo with ID %w doesn't exists in Todo Manager", id)
+	}
+
+	return todo, nil
+}
+
+func (t TodoManager) GetAll() error {
+	for _, todo := range t.Todos {
+		fmt.Printf("%v. %v\n%v\n", todo.id, todo.description, todo.createdAt)
+	}
+
+	return nil
+}
+
+func (t TodoManager) Delete(id int) error {
+	_, err := t.Get(id)
+	if err != nil {
+		return fmt.Errorf("Todo with ID %w doesn't exists in Todo Manager", id)
+	}
+
+	delete(t.Todos, id)
+	fmt.Printf("\nTodo with ID %w deleted succesfully", id)
+	return nil
+}
