18d2db1e24e52527db6a1806a4187cb108641df5
luis-octavius <luisoctavius.sc@gmail.com>
2025-10-29 21:21:26 -0300

    fix(commandHelp): add field usage in cliCommand to generate help in 'how to use the app'
    - add help in getCommands()
    - change commandHelp to generate usage instead of description of
      commands
    - fix error formatting in commandDelete(), commandAdd() and commandGet()
    - format time with RFC1123 in commandGet()

--- commands.go
+++ commands.go
@@ -5,11 +5,13 @@ import (
 	"os"
 	"strings"
 	"strconv"
+	"time"
 )
 
 type cliCommand struct {
 	name        string
 	description string
+	usage       string 
 	callback    func(args []string) error
 }
 
@@ -22,7 +24,7 @@ func commandExit(t *TodoManager, args []string) error {
 func commandHelp(t *TodoManager, args []string) error {
 	commands := getCommands(t)
 	for _, cmd := range commands {
-		fmt.Printf("%s: %s\n", cmd.name, cmd.description)
+		fmt.Printf("%s: %s\n", cmd.name, cmd.usage)
 	}
 	return nil
 }
@@ -32,7 +34,7 @@ func commandAdd(t *TodoManager, args []string) error {
 	fmt.Printf("Task description: %v\n", taskDescription)
 
 	err := t.Add(taskDescription); if err != nil {
-		fmt.Printf("error creating todo: %w", err)
+		return fmt.Errorf("error creating todo: %w\n", err)
 	}
 	
 	return nil
@@ -41,12 +43,12 @@ func commandAdd(t *TodoManager, args []string) error {
 func commandDelete(t *TodoManager, args []string) error {
 	taskId, err := strconv.Atoi(args[0])
 	if err != nil {
-		return fmt.Errorf("error in conversion: %w", err)
+		return fmt.Errorf("error in conversion: %w\n", err)
 	}
 	fmt.Printf("Task Id: %v\n", taskId)
 
 	err = t.Delete(taskId); if err != nil {
-		return fmt.Errorf("error deleting todo: %w", err)
+		return fmt.Errorf("error deleting todo: %w\n", err)
 	}
 	return nil
 }
@@ -60,7 +62,7 @@ func commandGet(t *TodoManager, args []string) error {
 		fmt.Printf("error getting todo: %w\n", err)
 	}
 
-	fmt.Printf("%v. %v\n%v\n", todo.id, todo.description, todo.createdAt)
+	fmt.Printf("%v. %v\n%v\n", todo.id, todo.description, todo.createdAt.Format(time.RFC1123))
 
 	return nil
 }
@@ -78,13 +80,23 @@ func getCommands(t *TodoManager) map[string]cliCommand {
 		"add": {
 			name:        "add",
 			description: "add a todo",
+			usage: "add study to algorithms proof (add a todo with the name 'study to algorithms proof')",
 			callback: func(args []string) error {
 				return commandAdd(t, args)
+			},
+		},
+		"help": {
+			name:        "help",
+			description: "shows how to use the commands",
+			usage: "",
+			callback: func(args []string) error {
+				return commandHelp(t, args)
 			},
 		},
 		"get-all": {
 			name:        "get all",
 			description: "show all todos",
+			usage: "get-all (shows all todos in the database)",
 			callback: func(args []string) error {
 				return commandGetAll(t, args)
 			},
@@ -92,6 +104,7 @@ func getCommands(t *TodoManager) map[string]cliCommand {
 		"get": {
 			name:        "get",
 			description: "get a todo by id",
+			usage: "get 1 (return the todo with id of 1)",
 			callback: func(args []string) error {
 				return commandGet(t, args)
 			},
@@ -99,6 +112,7 @@ func getCommands(t *TodoManager) map[string]cliCommand {
 		"delete": {
 			name:        "delete",
 			description: "delete a todo with the id",
+			usage: "delete 1 (delete a todo with id of 1)",
 			callback: func(args []string) error {
 				return commandDelete(t, args)
 			},
@@ -106,6 +120,7 @@ func getCommands(t *TodoManager) map[string]cliCommand {
 		"exit": {
 			name:        "exit",
 			description: "close the program",
+			usage: "exit (closes the program)",
 			callback: func(args []string) error {
 				return commandExit(t, args)
 			},
