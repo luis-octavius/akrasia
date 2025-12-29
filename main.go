966dfc4e4b4addfacc5d357af29f6d668c20f298
luis-octavius <luisoctavius.sc@gmail.com>
2025-12-29 15:58:41 -0300

    initialization of the database and config global variable

--- main.go
+++ main.go
@@ -1,11 +1,38 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/luis-octavius/akrasia/internal/database"
+	_ "modernc.org/sqlite"
 )
+
+var cfg = Config{}
 
 func main() {
 
-	fmt.Println("Welcome...")
-	startRepl()
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	dbPath := os.Getenv("DB_PATH")
+
+	db, err := sql.Open("sqlite", dbPath)
+	if err != nil {
+		log.Fatal("Error opening the database: ", err)
+	}
+
+	defer db.Close()
+
+	queries := database.New(db)
+	fmt.Println("Ok til here...")
+	cfg.Queries = queries
+
+	// initialize cobra root command
+	Execute()
 }
